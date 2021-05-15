ff4211b7bd83ed437f52fab08c454ee7ff1ad0eb
NikhilVooturi97 <nikhilvooturi57@gmail.com>
2021-05-15 12:59:44 +0530

    Commiting my first project to git

---
+++ OracleExample/src/main/java/com/oracle1/OracleExampleApplication.java
@@ -0,0 +1,13 @@
+package com.oracle1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OracleExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OracleExampleApplication.class, args);
+	}
+
+}
---
+++ OracleExample/src/test/java/com/oracle1/OracleExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oracle1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OracleExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
